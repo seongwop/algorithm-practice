464eae9ecba02e23fcca52ad348346d4196b92fb
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-09 03:36:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -3,6 +3,7 @@ class Solution {
     
         int cnt = 0;
         int zeros = 0;
+        int[] rank = {6, 6, 5, 4, 3, 2, 1};
         
         // 당첨 숫자와 숫자 0 따로 카운트
         for (int i = 0; i < lottos.length; i++) {
@@ -15,32 +16,7 @@ class Solution {
         }
 
         // 최고 순위는 0이 전부 당첨 번호, 최저 순위는 0이 전부 틀린 번호
-        int[] answer = {rank(cnt + zeros), rank(cnt)};
+        int[] answer = {rank[cnt + zeros], rank[cnt]};
         return answer;
-    }
-    
-    // 로또 순위 확인 메서드
-    public int rank(int num) {
-        int rank = 0;
-         switch (num) {
-            case 6 :
-                rank = 1;
-                break; 
-            case 5:
-                rank  = 2;
-                break;
-            case 4:
-                rank  = 3;
-                break;
-            case 3:
-                rank  = 4;
-                break;
-            case 2:
-                rank  = 5;
-                break;     
-            default :
-                rank  = 6;   
-        }
-        return rank;
     }
 }
