5f91cffe89567b1464b67daffc5ec01ec9d0acb7
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-08 22:39:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,22 +2,21 @@ class Solution {
     public int solution(int num) {
         
         int cnt = 0;
+        
         for (int i = 0; i < 500; i++) {
-            if (num == 1)
+            if (num == 1) // 주어진 수가 1인 경우
                 return 0;
-            if (num % 2 == 0) {
+            if (num % 2 == 0) { // 주어진 수가 짝수인 경우
                 num /= 2;
                 cnt++;
-            } else if (num % 2 == 1) {
+            } else if (num % 2 == 1) { // 주어진 수가 홀수인 경우
                 num = num * 3 + 1;
                 cnt++;
             }
-            if (num == 1) {
+            if (num == 1) { // 작업 후에 1이 된 경우
                 return cnt;
-            }
-            
+            }   
         }
-        
-        return -1;
+        return -1; // 500번 동안 1이 되지 않은 경우
     }
 }
