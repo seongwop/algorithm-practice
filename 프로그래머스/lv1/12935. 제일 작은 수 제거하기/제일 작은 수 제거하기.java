98ccd647edadd8d283ed15a1f357c81d546076e8
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-08 21:57:19 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        arr = new int[] {4,1,2,3};
+        
         // return이 빈 배열일 경우
         if (arr.length <= 1) {
             int[] answer = {-1};
@@ -16,21 +16,16 @@ class Solution {
                 min = arr[i];
             }
         }
-        System.out.println(min);
         
         int[] answer = new int[arr.length - 1];
-        int index1 = 0;
-        int index2 = 0;
+        int index = 0;
         
         // 최솟값을 제외한 원소들 복사
-        while (index1 < arr.length) {
-            if (arr[index1] == min) {
-                index1++;
-            }
-            answer[index2++] = arr[index1++];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min)
+                continue;
+            answer[index++] = arr[i];
         }
-        System.out.println(Arrays.toString(answer));
-        
         return answer;
     }
 }
