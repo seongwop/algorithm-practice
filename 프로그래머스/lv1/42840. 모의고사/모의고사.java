70e972869935232a6a4ad4de2cfcfb54c6be4c23
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-09 23:37:27 +0900

    [level 1] Title: 모의고사, Time: 0.60 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         
-
-
         int[] pattern_student1 = {1, 2, 3, 4, 5};
         int[] pattern_student2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] pattern_student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
@@ -15,50 +13,32 @@ class Solution {
             
             // 수포자 1 채점
             idx1 %= 5;
-            if (answers[i] == pattern_student1[idx1++])
-                student1++;
+            if (answers[i] == pattern_student1[idx1++]) student1++;
 
-            
             // 수포자 2 채점
             idx2 %= 8;
-            if (answers[i] == pattern_student2[idx2++])
-                student2++;
+            if (answers[i] == pattern_student2[idx2++]) student2++;
 
-            
             // 수포자 3 채점
             idx3 %= 10;
-            if (answers[i] == pattern_student3[idx3++])
-                student3++;
-
+            if (answers[i] == pattern_student3[idx3++]) student3++;
         }
+        
         int[] score = {student1, student2, student3};
-        int max = Math.max(student1, Math.max(student2, student3));
-        
-//         int cnt = 0;
-//         for (int s : score) {
-//             if (s == max)
-//                 cnt++;
-//         }
-//         int[] answer = new int[cnt];
-        
-//         for (int i = 0; i < 3; i++) {
-//             if (score[i] == max) {
-//                 cnt = i + 1;
-//                 answer[i] = cnt;
-//             }
-//         }
-        
+        int max = Math.max(student1, Math.max(student2, student3)); // 점수의 최댓값
         
         List<Integer> list = new ArrayList<>();
+        
+        // 최댓값을 받은 학생을 list에 add
         if (student1 == max) list.add(1);
         if (student2 == max) list.add(2);
         if(student3 == max) list.add(3);
             
+        // ArrayList를 배열로 변환
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++)
             answer[i] = list.get(i);
-        System.out.println(Arrays.toString(answer));
-        return answer;
 
+        return answer;
     }
 }
