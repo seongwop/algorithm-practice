d31941c8d5746659467322f00c801e33218bdc5f
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-10 15:12:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.31 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
 
+        // Comparator 인터페이스를 람다식으로 구현해서 정렬
         Arrays.sort(strings, (String a, String b) -> {
             if (a.charAt(n) == b.charAt(n))
                 return a.compareTo(b);
