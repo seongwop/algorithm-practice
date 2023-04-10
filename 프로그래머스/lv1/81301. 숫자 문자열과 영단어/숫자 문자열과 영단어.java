01ce3654ceb707179cbd1c735bcc6108f86262b8
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-11 00:37:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 8.87 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -55,7 +55,7 @@ class Solution {
         
 //         replace 메서드를 사용한 답
         
-//          s=s.replace("zero","0");
+//         s=s.replace("zero","0");
 //         s=s.replace("one","1");    
 //         s=s.replace("two","2");
 //         s=s.replace("three","3");
