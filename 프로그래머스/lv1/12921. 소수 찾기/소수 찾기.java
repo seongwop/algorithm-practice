95ff8b37e38a564e989b9c41bcee07aab49400e8
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-12 21:02:39 +0900

    [level 1] Title: 소수 찾기, Time: 16.35 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -8,10 +8,10 @@ class Solution {
         // 인덱스를 숫자 값으로 여기고 소수가 true로 저장될 배열 생성
         boolean[] bool = new boolean[n + 1];
         
-        for (int i = 2; i <= n; i++) {
+        // !! 제곱근까지만 나누어떨어지는지 확인하고 나누어떨어지지 않은 수들은 전부 소수
+        for (int i = 2; i <= (int)Math.sqrt(n) + 1; i++) {
             // 원소값이 false로 남아있다면 소수 
             if (bool[i] == false) {
-                answer++;
                 // 소수의 배수를 전부 true로 변경
                 for (int j = 2; j < n; j++) {
                     if (i * j > n) break;
@@ -19,6 +19,9 @@ class Solution {
                 }
             }
         }
+        // 배열의 false 개수를 셈
+        for (int i = 2; i <= n; i++)
+            if (bool[i] == false) answer++;
         return answer;
     }
 
