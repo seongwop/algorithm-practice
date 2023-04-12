50ad166446d968396e505b630490b60b2cb1fed9
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-12 15:19:26 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -4,11 +4,13 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         
+        // 1로 초기화된 배열에서 도난 당한 학생은 1을 빼고, 여벌이 있는 학생은 1을 더함
         int[] current = new int[n];
         Arrays.fill(current, 1);
         for (int idx : lost) current[idx - 1] -= 1;
         for (int idx : reserve) current[idx - 1] += 1;
         
+        // 붙어있는 인덱스를 조회하면서 서로 0, 2라면 1, 1로 만들어줌
         for (int i = 1; i < n; i++) {
             if (current[i] == 2 && current[i - 1] == 0) {
                 current[i]--;
