1f78f2fce916617faba4d0975dfba6f2d9e7bc9c
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-12 10:09:46 +0900

    [level 1] Title: 소수 찾기, Time: 14.78 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -3,12 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        // 에라토스테네스의 체 문제 - 소수의 배수를 전부 건너뛰면서 남은 숫자들이 소수
         
+        // 인덱스를 숫자 값으로 여기고 소수가 true로 저장될 배열 생성
         boolean[] bool = new boolean[n + 1];
         
         for (int i = 2; i <= n; i++) {
+            // 원소값이 false로 남아있다면 소수 
             if (bool[i] == false) {
                 answer++;
+                // 소수의 배수를 전부 true로 변경
                 for (int j = 2; j < n; j++) {
                     if (i * j > n) break;
                     bool[i * j] = true;
