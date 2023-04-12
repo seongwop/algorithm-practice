a7e9b7acc4b848eb7ccd7399a99d7109733014e8
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-12 10:05:54 +0900

    [level 1] Title: 소수 찾기, Time: 16.39 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        boolean[] bool = new boolean[n + 2];
+        boolean[] bool = new boolean[n + 1];
         
         for (int i = 2; i <= n; i++) {
             if (bool[i] == false) {
@@ -17,11 +17,11 @@ class Solution {
         }
         return answer;
     }
-}
-    
+
     // 해당 소수 찾기 메서드로는 효율성이 떨어짐
     // public boolean isPrime(int num) {
     //     for (int i = 2; i < num; i++) 
     //         if (num % i == 0) return false;
     //     return true;
     // } 
+}
