c29bf760c9b8a6f6465166c2df43c45a75815a6c
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-13 16:59:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -5,15 +5,19 @@ class Solution {
         int answer = 0;
 
         List<Integer> list = new ArrayList<>();
-
+        // 문자열을 캐릭터 배열로 쪼갬
         char[] charArray = dartResult.toCharArray();
+        
         for (int i = 0; i < charArray.length; i++) {
+            // 점수가 10일 경우 1, 0 과 보너스까지 계산해서 리스트에 추가
             if (charArray[i] == '1' && charArray[i + 1] == '0') {
                 list.add(calculate(10, charArray[i + 2]));
                 i += 2;
+            // 캐릭터가 숫자일 경우
             } else if (charArray[i] >= 48 && charArray[i] <= 57) {
                 list.add(calculate(charArray[i] - '0', charArray[i + 1]));
                 i += 1;
+            // 캐릭터가 옵션일 경우
             }  else if (charArray[i] == '*') {
                 if (list.size() < 2) {
                     list.set(0, list.get(0) * 2);
@@ -29,11 +33,10 @@ class Solution {
         for (Integer item : list) {
             answer += item;
         }
-    
         return answer;
     }
-
     
+    // 점수와 보너스가 주어지면 계산해서 반환
     public int calculate(int num, char ch) {
         if (ch == 'S') return (int)Math.pow(num, 1);
         else if (ch == 'D') return (int)Math.pow(num, 2);
