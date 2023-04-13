49f312fa639379bb8564d4e9970ee1ed479c7911
seongwop <93995037+seongwop@users.noreply.github.com>
2023-04-13 16:55:51 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.74 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -6,6 +6,7 @@ class Solution {
         
         int[][] array = new int[numbers.length][2];
 
+        // numbers 배열에 있는 숫자들을 2차원 배열에서의 위치로 바꿔서 새로운 2차원 배열에 전부 저장
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) {
                 array[i][0] = 3;
@@ -16,18 +17,20 @@ class Solution {
             }
         }
         
+        // 왼손가락과 오른손가락의 첫 시작 위치
         int[] currentL = {3, 0};
         int[] currentR = {3, 2};
         
         for (int i = 0; i < numbers.length; i++) {
+            // 1, 4, 7을 누를 경우 무조건 L
             if (Arrays.equals(array[i], new int[]{0, 0}) || Arrays.equals(array[i], new int[]{1, 0}) || Arrays.equals(array[i], new int[]{2, 0})) {
                 currentL = array[i];
                 answer += "L";
-
+            // 3, 6, 9를 누를 경우 무조건 R
             } else if (Arrays.equals(array[i], new int[]{0, 2}) || Arrays.equals(array[i], new int[]{1, 2}) || Arrays.equals(array[i], new int[]{2, 2})) {
                 currentR = array[i];
                 answer += "R";
-
+            // 가운데를 누를 경우  (왼손가락과 오른손가락으로부터 거리가 같을 경우, 한쪽이 더 클 경우)
             } else {
                 if (Math.abs(array[i][0] - currentL[0]) + Math.abs(array[i][1] - currentL[1])
                         == Math.abs(array[i][0] - currentR[0]) + Math.abs(array[i][1] - currentR[1])) {
