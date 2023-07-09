7ff6eeb8e84fbc3b84ca0a1756db32b3cf6889f8
박성우 <93995037+seongwop@users.noreply.github.com>
2023-07-10 00:56:58 +0900

    [level 2] Title: 기능개발, Time: 2.06 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/README.md
+++ 프로그래머스/lv2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java#) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 2.25 ms
+메모리: 72.5 MB, 시간: 2.06 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -7,13 +7,16 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
 
+        // Queue에 완성일 전부 넣기
         for (int i = 0; i < progresses.length; i++) {
             queue.add((int) Math.ceil((double) (100 - progresses[i]) / speeds[i]));
         }
         
+        // Queue가 빌 때까지
         while (!queue.isEmpty()) {
             int min = queue.poll();
             int count = 1;
+            // 처음 꺼낸 값이 가장 크거나 같을 동안 Queue에서 계속 꺼내면서 개수 카운트
             while(!queue.isEmpty() && min >= queue.peek()) {
                 queue.poll();
                 count++;
