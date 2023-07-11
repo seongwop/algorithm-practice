740255bbc0e471c15dcaa0455ebb0e02da191521
박성우 <93995037+seongwop@users.noreply.github.com>
2023-07-11 13:53:39 +0900

    [level 2] Title: 올바른 괄호, Time: 18.82 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,33 +2,18 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
-        StringBuilder sb = new StringBuilder(s);
-
         Stack<Character> stack = new Stack<>();
-
-        int stackSize = 0;
-
-        for (int i = 0; i < sb.length(); i++) {
-            char c = sb.charAt(i);
+        
+        for (char c : s.toCharArray()) {
             if (c == '(') {
                 stack.push(c);
-                stackSize++;
-            } else if (c == ')') {
-                if (stackSize == 0) {
-                    answer = false;
-                    break;
-                }
+            // "()" 가 맞춰질 때마다 pop
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
                 stack.pop();
-                stackSize--;
+            } else {
+                return false;
             }
         }
-
-        if (stackSize != 0) {
-            answer = false;
-        }
-
-        return answer;
+        return stack.isEmpty();
     }
 }
