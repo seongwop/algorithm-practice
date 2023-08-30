a49283a910bf4a921e7936ff17467c01bfd14d1b
Seongwoo Park <93995037+seongwop@users.noreply.github.com>
2023-08-30 21:51:12 +0900

    [level 3] Title: 등굣길, Time: 0.53 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -11,20 +11,14 @@ class Solution {
         
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (route[i][j] == 0) {
-                    if (route[i - 1][j] == -1 && route[i][j - 1] != -1) {
-                        route[i][j] = route[i][j - 1];
-                    } else if (route[i - 1][j] != -1 && route[i][j - 1] == -1) {
-                        route[i][j] = route[i - 1][j];
-                    } else if (route[i - 1][j] == -1 && route[i][j - 1] == -1) {
-                        route[i][j] = 0;
-                    } else {
-                        route[i][j] = route[i - 1][j] + route[i][j - 1];
-                    }
-                    if (route[i][j] >= 1000000007) {
-                        route[i][j] = route[i][j] % 1000000007;
-                    }
+                if (i == 1 && j == 1) {
+                    continue;
                 }
+                if (route[i][j] == -1) {
+                    route[i][j] = 0;
+                    continue;
+                } 
+                route[i][j] = (route[i - 1][j] + route[i][j - 1]) % 1000000007;
             }
         }
         return route[m][n];
