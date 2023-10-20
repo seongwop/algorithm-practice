bec3c3e3b8af016ec3bc88fa2c6b0b934e40fdb0
Seongwoo Park <93995037+seongwop@users.noreply.github.com>
2023-10-21 06:28:20 +0900

    [level 3] Title: 기지국 설치, Time: 0.67 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -6,16 +6,18 @@ class Solution {
         int start = 1;
         
         for (int i = 0; i < stations.length; i++) {
-            if (stations[i] - w > start) {
-                if ((stations[i] - w - start) % (w * 2 + 1) == 0) {
-                    answer += (stations[i] - w - start) / (w * 2 + 1);
-                } else {
-                    answer += (stations[i] - w - start) / (w * 2 + 1) + 1;
-                }
-            } 
+            if (stations[i] - w <= start) {
+                start = stations[0] + w + 1;
+                continue;
+            }
+            if ((stations[i] - w - start) % (w * 2 + 1) == 0) {
+                answer += (stations[i] - w - start) / (w * 2 + 1);
+            } else {
+                answer += (stations[i] - w - start) / (w * 2 + 1) + 1;
+            }
             start = stations[i] + w + 1;
         }
-        if (start <= n) {
+        if (start < n) {
             if ((n - start + 1) % (w * 2 + 1) == 0) {
                 answer += (n - start + 1) / (w * 2 + 1);
             } else {
