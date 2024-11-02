a0f46cde440fee518ad8297bf67b6419be14815e
Seongwoo Park <93995037+seongwop@users.noreply.github.com>
2024-11-02 05:23:55 +0000

    15989. 1, 2, 3 더하기 4

---
+++ 백준/Gold/15989. 1, 2, 3 더하기 4/main15989.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main15989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+        dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
+
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3] + "\n");    
+        }
+        System.out.println(sb);
+    }
+}
+
