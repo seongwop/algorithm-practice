c92ac6d3d94dbf052ccb538c8f3551b57db52ce7
Seongwoo Park <93995037+seongwop@users.noreply.github.com>
2026-04-07 10:33:48 +0900

    [level 2] Title: 타겟 넘버, Time: 6.50 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165.타겟 넘버/타겟 넘버.java
@@ -0,0 +1,16 @@
+class Solution {
+        static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int cur, int target, int total) {
+        if (cur == numbers.length) {
+            if (target == total) answer++;
+            return;
+        }
+        dfs(numbers, cur + 1, target, total + numbers[cur]);
+        dfs(numbers, cur + 1, target, total - numbers[cur]);
+    }
+}
